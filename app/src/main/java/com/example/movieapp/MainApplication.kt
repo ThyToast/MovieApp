55ec03dbcb3b2d58061e421d9c0782cbf0ee18de
Lee Hangsen <hangsen.lee@hexainitiative.com>
2022-07-27 16:05:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp
+
+import android.app.Application
+
+class MainApplication : Application() {
+    // initialize dependency injection here
+}
---
+++ app/src/main/java/com/example/movieapp/api/MovieService.kt
@@ -0,0 +1,5 @@
+package com.example.movieapp.api
+
+interface MovieService {
+    //TODO: Add GET requests
+}
