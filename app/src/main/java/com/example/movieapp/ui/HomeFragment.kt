9f1ba38303af23abeaae109fd3b28901c180cf73
Lee Hangsen <hangsen.lee@hexainitiative.com>
2022-07-29 17:43:29 +0800

    Initial UI setup, added designs for home fragment and recyclerview items

--- app/src/main/java/com/example/movieapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/HomeFragment.kt
@@ -1,20 +1,48 @@
 package com.example.movieapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.movieapp.R
+import androidx.fragment.app.Fragment
+import com.example.movieapp.databinding.FragmentHomeBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val movieFilter = arrayOf("By Letter", "By Date")
+        binding.fabFilterMovie.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Filter Movie")
+                .setSingleChoiceItems(
+                    movieFilter, 0
+                ) { dialog, which ->
+                    {
+                     // dialog onClick()
+                    }
+                }
+                .setNegativeButton("Cancel") { _, _ -> }
+                .show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
