511bc953214f164d692fc279744315a7b7fe47cb
Lee Hangsen <hangsen.lee@hexainitiative.com>
2022-09-04 14:37:45 +0800

    Fixed missing setContentView and updated android manifest

--- app/src/main/java/com/example/movieapp/presentation/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/base/BaseActivity.kt
@@ -1,17 +1,17 @@
 package com.example.movieapp.presentation.ui.base
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<T : ViewBinding> : Activity() {
+abstract class BaseActivity<T : ViewBinding> : AppCompatActivity() {
     private lateinit var binding: T
-
     abstract val bindingInflater: (LayoutInflater) -> T
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = bindingInflater.invoke(layoutInflater)
+        setContentView(binding.root)
     }
 }
